b1d5fb15f514a846f12a10e28df9f3c9b9e8afe9
TrashBin <96432924+Trashbin7@users.noreply.github.com>
2023-04-13 19:08:30 +0300

    i forgot to commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# made by github.com/TrashBin7
+# if there is a problem, dm me at "mertcinarsah#5188" in discord
+# read readme.md for more info
 import time
 import random
 import os
@@ -8,41 +11,7 @@ choice = 0
 userhp = 0
 enemyhp = 0
 gameloop = 1
-
-def loading(value):
-    if value == "start":
-        time.sleep(0.5)
-        for i in range(2):
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[----] Loading...")
-            time.sleep(0.025)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[=---] Loading...")
-            time.sleep(0.025)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[==--] Loading...")
-            time.sleep(0.025)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[===-] Loading...")
-            time.sleep(0.025)            
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[====] Loading...")
-            time.sleep(0.025)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[-===] Loading...")
-            time.sleep(0.025)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[--==] Loading...")
-            time.sleep(0.025)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[---=] Loading...")
-            time.sleep(0.025)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("[----] Loading...")
-            time.sleep(0.2)
-        time.sleep(1)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("[----] Loading done.")
+username = ""
 
 def slowprint(string):
     for letter in string:
@@ -56,13 +25,15 @@ def fastprint(string):
 
 
 while loop == 1:
-    print("--------------------------------------------------")
-    slowprint("Welcome back to Battle Simulator!\n\n\n")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if username == "":
+        slowprint("Welcome back to Battle Simulator!\n\n\n")
+    else: 
+        slowprint(f"Welcome back to Battle Simulator, {username}!\n\n\n")
+
     fastprint("Choose Your Difficulty:\n[1] - Baby Mode\n[2] - Easy Mode\n[3] - Normal Mode\n[4] - Hard Mode\n[5] - Chad Mode\n\n\n")
     
     choice = input("Pick one: ")
-    os.system('cls' if os.name == 'nt' else 'clear')
-    time.sleep(2)
     
     choice = str(choice)
 
@@ -88,13 +59,91 @@ while loop == 1:
         time.sleep(3)
         userhp = 100
         enemyhp = 100
+    if username == "":
+        username = input("Username: ")
+    else:
+        namechange = input(f"Username (leave blank to continue with {username}): ")
+        if namechange == "":
+            print(f"welcome back, {username}")
+        else:
+            username = namechange
+            print(f"username set as {username}")
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("Setting up the game...")
     time.sleep(1)
     gameloop = 1
     while gameloop == 1:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("It's your turn.\n\n[1] Attack - [2] Defend - [3] Heal")
-        
+        print(f"It's your turn.\n{username}: {userhp} hp | enemy: {enemyhp} hp\n\n[1] Attack - [2] Defend - [3] Heal\n\n")
+        useranswer = input()
+        defend = False
+        if useranswer == "1":
+            critical = random.randint(1, 10)
+            time.sleep(0.5)
+            if critical >= 8:
+                print("Critical hit? ", end="")
+                time.sleep(0.5)
+                slowprint("YES!")
+                dmg = random.randint(10, 12)
+            else:
+                print("Critical hit? ", end="")
+                time.sleep(0.5)
+                slowprint("no.")
+                dmg = random.randint(2, 10)
+            print()
+            time.sleep(0.5)
+            slowprint(f"You dealt {dmg} damage to the enemy.")
+            enemyhp = enemyhp - dmg
+            dmg = 0
+            time.sleep(1)
+        if useranswer == "2":
+            print("You will block all enemy damages.")
+            defend = True
+            time.sleep(1)
+
+        if useranswer == "3":
+            healamount = random.randint(1, 10)
+            print(f"you healed {healamount}")
+            userhp = userhp + healamount
+            time.sleep(1)
 
 
-        enemymove = random.randint(1, 3)        #todo
+        print(f"\nEnemy turn\n\n")
+        enemymove = random.randint(1, 2)
+        if enemymove == 1:
+            critical = random.randint(1, 10)
+            time.sleep(0.5)
+            if critical >= 8:
+                print("Critical hit? ", end="")
+                time.sleep(0.5)
+                slowprint("YES!")
+                dmg = random.randint(10, 12)
+            else:
+                print("Critical hit? ", end="")
+                time.sleep(0.5)
+                slowprint("no.")
+                dmg = random.randint(2, 10)
+            print()
+            time.sleep(0.5)
+            if defend == True:
+                print("enemy attacked to you, but you had shield!")
+            else:
+                print(f"enemy dealt {dmg} to you!")
+                userhp = userhp - dmg
+            time.sleep(1)
+        if enemymove == 2:
+            healamount = random.randint(1, 10)
+            print(f"enemy healed {healamount}")
+            enemyhp = enemyhp + healamount
+            time.sleep(1)
+        time.sleep(1)
+        if userhp <= 0 or enemyhp <= 0:
+            gameloop = 0
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"{username}: {userhp} hp | enemy: {enemyhp} hp\n")
+    if enemyhp <= userhp:
+        print("you Won!")
+    else:
+        print("You Lost!")
+    print("Press [ENTER] to play again.\n\n\nmade by github.com/TrashBin7")
+    input()
