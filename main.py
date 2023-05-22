7916c0e3a1e6790a159a13e8142e2e34e6c55b37
TrashBin <96432924+Trashbin7@users.noreply.github.com>
2023-05-22 20:14:21 +0300

    [UNSTABLE] tried adding ulti

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-# made by github.com/TrashBin7
-# if there is a problem, dm me at "mertcinarsah#5188" in discord
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                 #
+#                                                                 #
+#                     Battle Simulator                            #
+#                made by github.com/TrashBin7                     #
+#                                                                 #
+#                                                                 #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
+#
+# this is one of the biggest projects i've made so far (i am a beginner).
 # read readme.md for more info
-import time
-import random
-import os
+# 19/4/2023 (DD/MM/YYYY)
+#
+#
+#
 
+
+import os
+import random
+import time
 
 loop = 1
 choice = 0
@@ -12,6 +27,15 @@ userhp = 0
 enemyhp = 0
 gameloop = 1
 username = ""
+remaining = 3
+ultiname = ""
+ultiid = 0
+ultidesc = 0
+playerturn = 0
+pickedultimate = 0
+ultianswer = 0
+
+
 
 def slowprint(string):
     for letter in string:
@@ -21,6 +45,47 @@ def slowprint(string):
 def fastprint(string):
     for letter in string:
         print(letter, end='', flush=True)
+
+def randomulti():
+    ultiid = random.randint(1, 6)
+    if ultiid == 1:
+        ultiname = "Meteor Attack"
+        ultidesc = "A powerful attack that rains down meteors from the sky, dealing massive damage to all enemies."
+    elif ultiid == 2:
+        ultiname = "Nova Blast"
+        ultidesc = "A burst of energy that creates a massive explosion, dealing damage to all enemies in a wide area."
+    elif ultiid == 3:
+        ultiname = "Ultimate Healing"
+        ultidesc = "A powerful healing ability that restores a large amount of health."
+    elif ultiid == 4:
+        ultiname = "Time Warp" 
+        ultidesc = "Slows down your opponents' movements, giving you a chance to attack multiple times in a row."
+    elif ultiid == 5:
+        ultiname = "Mind Control"
+        ultidesc = "Take over your opponent's mind, forcing them to attack their own teammates."
+    elif ultiid == 6:
+        ultiname = "Rick Astley's Shield"
+        ultidesc = "Creates a powerful shield that absorbs all incoming damage."
+    else:
+        print("ERROR: no ultimate found")
+    pickedultimate = 1
+
+
+def ultimate():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"------------------------------\n\nName: {ultiname}\nDescription: {ultidesc}\nId: {ultiid}\n\nWARNING: you cannot play this round if you use your ultimate attack!\n\n[1] Use  -  [2] Cancel\n\n------------------------------\n\n")
+    ultianswer = input("1-2: ")
+    if ultianswer == "1":
+        if ultiid == 1:
+            dmg = random.randint(7, 12)
+            print(f"You dealt {dmg} damage to the enemy.")
+            enemyhp = enemyhp - dmg
+            ultimatedmg = enemyhp
+            return enemyhp
+        if ultiid == 2:
+
+
+
 
 
 
@@ -70,46 +135,78 @@ while loop == 1:
             print(f"username set as {username}")
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Setting up the game...")
-    time.sleep(1)
+    randomulti()
+    time.sleep(0.5)
+
+
     gameloop = 1
     while gameloop == 1:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print(f"It's your turn.\n{username}: {userhp} hp | enemy: {enemyhp} hp\n\n[1] Attack - [2] Defend - [3] Heal\n\n")
-        useranswer = input()
-        defend = False
-        if useranswer == "1":
-            critical = random.randint(1, 10)
-            time.sleep(0.5)
-            if critical >= 8:
-                print("Critical hit? ", end="")
-                time.sleep(0.5)
-                slowprint("YES!")
-                dmg = random.randint(10, 12)
-            else:
-                print("Critical hit? ", end="")
-                time.sleep(0.5)
-                slowprint("no.")
-                dmg = random.randint(2, 10)
-            print()
-            time.sleep(0.5)
-            slowprint(f"You dealt {dmg} damage to the enemy.")
-            enemyhp = enemyhp - dmg
-            dmg = 0
-            time.sleep(1)
-        if useranswer == "2":
-            print("You will block all enemy damages.")
-            defend = True
-            time.sleep(1)
+        print(f"It's your turn.\n{username}: {userhp} hp | enemy: {enemyhp} hp\n\n[1] Attack - [2] Defend - [3] Heal")
+        if remaining == 0:
+            print(f"[4] Ultimate: {ultiname} (Ready)\n\n")
+        else:
+            print(f"[-] Ultimate: {ultiname} ({remaining} rounds left)\n\n")
 
-        if useranswer == "3":
-            healamount = random.randint(1, 10)
-            print(f"you healed {healamount}")
-            userhp = userhp + healamount
-            time.sleep(1)
+        playerturn = 1
+        while playerturn == 1:
+            useranswer = input()
+            defend = False
+
+            if useranswer == "1":
+                critical = random.randint(1, 10)
+                time.sleep(0.5)
+                if critical >= 8:
+                    print("Critical hit? ", end="")
+                    time.sleep(0.5)
+                    slowprint("YES!")
+                    dmg = random.randint(10, 12)
+                else:
+                    print("Critical hit? ", end="")
+                    time.sleep(0.5)
+                    slowprint("no.")
+                    dmg = random.randint(2, 10)
+                print()
+                time.sleep(0.5)
+                slowprint(f"You dealt {dmg} damage to the enemy.")
+                enemyhp = enemyhp - dmg
+                dmg = 0
+                playerturn = 0
+                time.sleep(1)
+                if remaining > 0:
+                    remaining = remaining - 1
+
+            if useranswer == "2":
+                print("You will block all enemy damages.")
+                defend = True
+                time.sleep(1)
+                playerturn = 0
+                if remaining > 0:
+                    remaining = remaining - 1
+
+            if useranswer == "3":
+                healamount = random.randint(1, 10)
+                print(f"you healed {healamount}")
+                userhp = userhp + healamount
+                time.sleep(1)
+                playerturn = 0
+                if remaining > 0:
+                    remaining = remaining - 1
+            
+            if useranswer == "4":
+                if remaining > 0:
+                    print("Your ultimate attack isn't ready...")
+                    time.sleep(0.5)
+                    print("It's still your turn!")
+                else:
+                    ultimate()
+            
 
 
         print(f"\nEnemy turn\n\n")
         enemymove = random.randint(1, 2)
+
+
         if enemymove == 1:
             critical = random.randint(1, 10)
             time.sleep(0.5)
@@ -131,8 +228,10 @@ while loop == 1:
                 print(f"enemy dealt {dmg} to you!")
                 userhp = userhp - dmg
             time.sleep(1)
+
+
         if enemymove == 2:
-            healamount = random.randint(1, 10)
+            healamount = random.randint(1,10)
             print(f"enemy healed {healamount}")
             enemyhp = enemyhp + healamount
             time.sleep(1)
